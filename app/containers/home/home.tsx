bfdbe16edae541bcaceccbd6e08a37eb354e16aa
Caio Oliveira <colive.dev@gmail.com>
2024-10-09 22:51:15 -0300

    tabs 100%

--- app/containers/home/home.tsx
+++ app/containers/home/home.tsx
@@ -6,6 +6,7 @@ import HomeHeader from "./presentations/Header";
 import HomeDashboard from "./presentations/Dashboard";
 import HomeFooter from "./presentations/Footer";
 import { IActivity } from "@/types/activity";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function HomeContainer() {
   const {
@@ -30,27 +31,36 @@ export default function HomeContainer() {
       <HomeHeader />
       <div className="m-3 md:flex md:m-0">
         <div className="w-full mb-3 md:w-2/4 md:mr-3 md:mb-0">
-          <ActivityCardTable
-            className="mb-3"
-            activities={dailyActivities}
-            type="daily"
-            onToggle={(id) => toggleActivityDone(id)}
-            onToggleAll={onToggleAll}
-          >
-            We have selected some important daily tasks that you may want to
-            consider.
-          </ActivityCardTable>
-          <ActivityCardTable
-            activities={weeklyActivities}
-            type="weekly"
-            onToggle={(id) => toggleActivityDone(id)}
-            onToggleAll={onToggleAll}
-          >
-            We have also selected some key weekly tasks that you may find
-            useful.
-          </ActivityCardTable>
+          <Tabs defaultValue="daily">
+            <TabsList className="grid w-full grid-cols-2 md:mx-2">
+              <TabsTrigger value="daily">Daily</TabsTrigger>
+              <TabsTrigger value="weekly">Weekly</TabsTrigger>
+            </TabsList>
+            <TabsContent value="daily">
+              <ActivityCardTable
+                className="mb-3"
+                activities={dailyActivities}
+                type="daily"
+                onToggle={(id) => toggleActivityDone(id)}
+                onToggleAll={onToggleAll}
+              >
+                We have selected some important daily tasks that you may want to
+                consider.
+              </ActivityCardTable>
+            </TabsContent>
+            <TabsContent value="weekly">
+              <ActivityCardTable
+                activities={weeklyActivities}
+                type="weekly"
+                onToggle={(id) => toggleActivityDone(id)}
+                onToggleAll={onToggleAll}
+              >
+                We have also selected some key weekly tasks that you may find
+                useful.
+              </ActivityCardTable>
+            </TabsContent>
+          </Tabs>
         </div>
-
         <div className="w-full md:w-2/4">
           <div className="w-3/3 md:ml-2">
             <HomeDashboard
